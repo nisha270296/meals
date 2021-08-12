a91465a8b6328b1727c800605ba6591028337037
nisha1996 <nisha@pietechsolution.com>
2021-08-12 20:15:03 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'meals';
+  showList = false;
+  mealList :any = [];
+  categories :any = [];
+  searchbyname="";
+  searchbyfirstletter="";
+  
+  constructor(public http:HttpClient) {
+  }
+
+  searchByName(searchTerm:any){
+    this.getData('https://www.themealdb.com/api/json/v1/1/search.php?s='+searchTerm).subscribe((res: any) => {
+      console.log(res);
+      this.showList = true;
+      this.mealList = res.meals
+    })
+  }
+  
+  searchByFirstLetter(searchTerm:any){
+    this.getData('https://www.themealdb.com/api/json/v1/1/search.php?f='+searchTerm).subscribe((res: any) => {
+      console.log(res);
+      this.showList = true;
+      this.mealList = res.meals
+    })
+  }
+
+  getallMeals(){
+    this.getData('https://www.themealdb.com/api/json/v1/1/categories.php').subscribe((res: any) => {
+      console.log(res);
+      this.categories = res.categories
+    })
+  }
+  
+  // sends request to fetch data from server
+  public getData(url:string): Observable<any> {
+    return this.http.get(url);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -8,9 +10,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
   ],
-  providers: [],
+  providers: [HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
